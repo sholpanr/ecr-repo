bb01d0e39b532ef4b50e393d91954e3d78d4da91
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-03-26 17:05:06 -0500

    added ecr

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "nodejs-app" 
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
