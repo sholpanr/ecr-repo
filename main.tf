bfd473ae0b9385f464211052d7b7bc1915738bac
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-03-27 17:12:23 -0500

    v2

--- main.tf
+++ main.tf
@@ -33,12 +33,12 @@ terraform {
 #     }
 # }
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.0.0"
   
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 3.0, < 4.0"
     }
   }
 }
