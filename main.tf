0a306149918ad92e8625f181fb25195d4f68aa98
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-03-27 15:25:49 -0500

    changed bucket

--- main.tf
+++ main.tf
@@ -7,31 +7,31 @@ terraform {
     # shared_credentials_file = "~/.aws/credentials"
   }
 }
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tf-state-aksholpan-2"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "tf-state-aksholpan-2"
+#   # Enable versioning so we can see the full revision history of our
+#   # state files
+#   versioning {
+#     enabled = true
+#   }
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "tf-locks-aksholpan-2"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-    }
-}
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "tf-locks-aksholpan-2"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#     }
+# }
 
 
 provider "aws" {
