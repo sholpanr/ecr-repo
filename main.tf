07507b1a057b17db53499fa6ee91e610afab9d81
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-03-27 15:07:41 -0500

    added s3

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket                  = "tf-state-aksholpan"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+    # shared_credentials_file = "~/.aws/credentials"
+  }
+}
+
+
+
 provider "aws" {
     region = "us-east-1"
     # access_key = var.AWS_ACCESS_KEY_ID
