f3f1f06775c23fb248698b32c3030236b2d21045
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-03-27 17:08:16 -0500

    added version

--- main.tf
+++ main.tf
@@ -32,7 +32,16 @@ terraform {
 #     type = "S"
 #     }
 # }
-
+terraform {
+  required_version = ">= 0.14"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
