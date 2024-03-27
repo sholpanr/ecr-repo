08ee63ca45665320660a5122466be0950d7f155a
aksholpan ramazanova <aksholpan.ramazanova@gmail.com>
2024-03-27 17:32:57 -0500

    fixed

--- main.tf
+++ main.tf
@@ -4,37 +4,11 @@ terraform {
     key    = "my-terraform-project"
     region = "us-east-1"
     # dynamodb_table          = "tf-locks-aksholpan-2"
-    # shared_credentials_file = "~/.aws/credentials"
   }
 }
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "tf-state-aksholpan-2"
-#   # Enable versioning so we can see the full revision history of our
-#   # state files
-#   versioning {
-#     enabled = true
-#   }
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
 
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "tf-locks-aksholpan-2"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#     }
-# }
 terraform {
   required_version = ">= 1.0.0"
-  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -45,8 +19,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  # access_key = var.AWS_ACCESS_KEY_ID
-  # secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_ecr_repository" "my_ecr_repo" {
